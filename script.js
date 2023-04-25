eb44cdf1041acdf63e4fd56d7b4598cba70e8184
Daxi90 <hdateam90@gmail.com>
2023-04-25 09:20:31 +0200

    Added Image in Overview Site

--- script.js
+++ script.js
@@ -11,24 +11,35 @@ async function loadPokedex() {
   renderPokemonIndex(currentPokemon);
 }
 
-function renderPokemonIndex(currentPokemon) {
+async function getSinglePokemonOverviewImage(url) {
+  let response = await fetch(url);
+  let pokemonSingleData = await response.json();
+  let imageUrl =
+    pokemonSingleData["sprites"]["other"]["official-artwork"]["front_default"];
+  console.log(imageUrl);
+  return imageUrl;
+}
 
-    let pokedex = document.getElementById('pokedex');
+async function renderPokemonIndex(currentPokemon) {
+  let pokedex = document.getElementById("pokedex");
   for (let i = 0; i < currentPokemon["results"].length; i++) {
     const pokemon = currentPokemon["results"][i];
+
+    //RENDER IMAGE FROM POKEMON
+    let imageUrl = await getSinglePokemonOverviewImage(pokemon["url"]);
+
     console.log(currentPokemon);
     pokedex.innerHTML += /*html*/ `
 
-            <div class="card" id="pokemon-${pokemon["name"]} style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${pokemon["name"]}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Details</a>
-            </div>
+            <div class="card col-lg-3 col-12" id="pokemon-${pokemon["name"]} style="width: 18rem;">
+              <img src="${imageUrl}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title text-capitalize">${pokemon["name"]}</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Details</a>
+              </div>
             </div>        
         `;
-    // FOR LOOP RENDER IMAGE FROM POKEMON 
   }
 
   loadMoreUrl = currentPokemon["next"];
@@ -36,7 +47,7 @@ function renderPokemonIndex(currentPokemon) {
 }
 
 async function loadMore(loadMoreUrl) {
-    let response = await fetch(loadMoreUrl);
-    nextPokemon = await response.json();
-    renderPokemonIndex(nextPokemon);
-  }
+  let response = await fetch(loadMoreUrl);
+  nextPokemon = await response.json();
+  renderPokemonIndex(nextPokemon);
+}
