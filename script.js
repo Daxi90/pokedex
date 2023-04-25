079b0784d29cf64b3492f8074dc9fde5bf98bdab
Daxi90 <hdateam90@gmail.com>
2023-04-25 16:33:30 +0200

    Replaced bootstrap modal with own modal

--- script.js
+++ script.js
@@ -52,45 +52,20 @@ async function loadMore(loadMoreUrl) {
   renderPokemonIndex(nextPokemon);
 }
 
+function closeModal(){
+  document.getElementById('modal').classList.add('d-none');
+}
+
 function renderModal(name) {
   let modalContainer = document.getElementById("modal-container");
-  modalContainer.innerHTML = '';
+  modalContainer.innerHTML = "";
 
   modalContainer.innerHTML = /*html*/ `
   <!-- Modal -->
-  <div
-    class="modal fade"
-    id="exampleModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">
-            ${name}
-          </h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">...</div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Close
-          </button>
-        </div>
-      </div>
+  <div onclick="closeModal()" id="modal" class="modal-background-container">
+    <div class="modal-container">
+      <span>${name}</span>
     </div>
   </div>
-
   `;
 }
